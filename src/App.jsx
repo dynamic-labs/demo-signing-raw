4d4bd2bf723baf4393c833edfd0e4ac643996d50
Yoni Goldberg <yoni@dynamic.xyz>
2023-12-27 18:52:31 +0000

    feat: Update React and SDK dependencies
    Updated React and SDK dependencies, added new wallet connector extension.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { DynamicContextProvider, DynamicWidget, FilterAndSortWallets } from '@dynamic-labs/sdk-react-core';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+  FilterAndSortWallets,
+} from "@dynamic-labs/sdk-react-core";
 
-import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
-import Inner from './Inner';
-import { EthersExtension } from '@dynamic-labs/ethers-v5';
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import Inner from "./Inner";
+import { EthersExtension } from "@dynamic-labs/ethers-v5";
 
 function App() {
   return (
     <DynamicContextProvider
-    settings={{
-      // yoni-tesing yoni-demo
-      environmentId: '71bfa05e-f4dc-43a4-98d8-94befd4555a2',
-      walletConnectors: [EthereumWalletConnectors],
-      walletConnectorExtensions: [EthersExtension]
-    }}
-  >
-
+      settings={{
+        // yoni-tesing yoni-demo
+        environmentId: "71bfa05e-f4dc-43a4-98d8-94befd4555a2",
+        walletConnectors: [EthereumWalletConnectors],
+        walletConnectorExtensions: [EthersExtension],
+      }}
+    >
       <DynamicWidget />
-    <Inner/>
-      Hello
-  </DynamicContextProvider>
-  )
+      <Inner />
+    </DynamicContextProvider>
+  );
 }
 
-export default App
+export default App;
